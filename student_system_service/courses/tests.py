ef33f991b1aaa2a3b3b9131e52d5d020e5d4b646
LizardBob <krzysztof.chrusciel@tivix.com>
2021-05-13 19:22:51 +0200

    add test_get_all_faculties_query, test_get_faculty_by_id_query

--- student_system_service/courses/tests.py
+++ student_system_service/courses/tests.py
@@ -1,5 +1,11 @@
-from ..conftest import *  # noqa
+import json
+
+import pytest
+
 from student_system_service.courses.consts import CourseType
+from student_system_service.courses.models import Faculty, Grade
+
+from ..conftest import *  # noqa
 
 
 @pytest.mark.django_db
@@ -16,3 +22,66 @@ def test_course_model_create(simple_courses, simple_faculty, simple_grade):
             i.id for i in Grade.objects.all()
         ]
         assert simple_course.student_set.exists()
+
+
+@pytest.mark.django_db
+def test_get_all_faculties_query(client_query, simple_faculties):
+    operation_name = "allFaculties"
+    response = client_query(
+        """
+        query allFaculties {
+          allFaculties {
+            id
+            name
+            students {
+              id
+              indexCode
+            }
+          }
+        }
+        """,
+        op_name=operation_name,
+    )
+
+    content = json.loads(response.content)
+    assert "errors" not in content
+    res_json = content.get("data").get(operation_name)
+
+    all_faculties = Faculty.objects.all()
+
+    for faculty in res_json:
+        expected_faculty = all_faculties.get(id=faculty.get("id"))
+        assert faculty.get("id") == str(expected_faculty.id)
+        assert faculty.get("name") == expected_faculty.name
+        assert faculty.get("students") == list(expected_faculty.students.all())
+
+
+@pytest.mark.django_db
+def test_get_faculty_by_id_query(client_query, simple_faculties):
+    faculty = simple_faculties[-1]
+    operation_name = "facultyById"
+    response = client_query(
+        """
+        query facultyById($id: String!) {
+          facultyById(id: $id) {
+            id
+            name
+            students {
+              id
+              username
+              name
+            }
+          }
+        }
+        """,
+        op_name=operation_name,
+        variables={"id": faculty.id},
+    )
+    content = json.loads(response.content)
+    assert "errors" not in content
+
+    result = content.get("data").get(operation_name)
+
+    assert result.get("id") == str(faculty.id)
+    assert result.get("name") == faculty.name
+    assert result.get("students") == list(faculty.students.all())
